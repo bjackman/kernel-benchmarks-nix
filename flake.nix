89be3e1e8bee2f675827c892cdbb83239dc900f3
Brendan Jackman <jackmanb@google.com>
2025-08-18 14:14:38 +0000

    Init with Nix boilerplate

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter."${system}" = pkgs.nixfmt-tree;
+      devShells."${system}".default = pkgs.mkShell {
+        packages = [ pkgs.nixos-rebuild ];
+      };
+    };
+}
