42e791977a76ca43c3fd05ceb96ce2bfd7aba760
Brendan Jackman <jackmanb@google.com>
2026-01-20 15:56:14 +0000

    dummy example of benchmark I wanna run

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=25.11";
+    firecracker = {
+      url = "github:firecracker-microvm/firecracker?ref=feature/secret-hiding";
+      flake = false;
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-    }:
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -16,7 +21,7 @@
       packages."${system}" = rec {
         benchmarks.firecracker-perf-tests =
           pkgs.callPackage ./packages/benchmarks/firecracker-perf-tests
-            { };
+            { inherit inputs; };
       };
 
       formatter."${system}" = pkgs.nixfmt-tree;
