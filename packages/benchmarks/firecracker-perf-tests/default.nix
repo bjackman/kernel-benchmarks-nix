7b341053864326fe026c016e82e59a938c2f6ffd
Brendan Jackman <jackmanb@google.com>
2026-01-21 16:55:59 +0000

    firecracker-perf-tests: Misc fixes

--- packages/benchmarks/firecracker-perf-tests/default.nix
+++ packages/benchmarks/firecracker-perf-tests/default.nix
@@ -13,7 +13,7 @@ pkgs.callPackage ../../wrap-benchmark.nix {
   inherit name;
   rawBenchmark = pkgs.writeShellApplication {
     inherit name;
-    runtimeInputs = with pkgs; [ git ];
+    runtimeInputs = with pkgs; [ git getopt ];
     text = builtins.readFile ./firecracker-perf-tests.sh;
     runtimeEnv.FIRECRACKER_REV = inputs.firecracker.rev;
   };
