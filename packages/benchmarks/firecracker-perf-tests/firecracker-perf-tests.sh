642a2c208031e36a0ae11ac9ee8b4d7d1d41b2e7
Brendan Jackman <jackmanb@google.com>
2026-01-21 16:00:13 +0000

    firecracker-perf-tests: Add --out-dir flag

--- packages/benchmarks/firecracker-perf-tests/firecracker-perf-tests.sh
+++ packages/benchmarks/firecracker-perf-tests/firecracker-perf-tests.sh
@@ -1,4 +1,49 @@
-set -eux -o pipefail
+set -eu -o pipefail
+
+#
+# Stupid args boilerplate
+#
+
+OUT_DIR=
+
+PARSED_ARGUMENTS=$(getopt -o o: --long out-dir: -- "$@")
+
+# shellcheck disable=SC2181
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to parse arguments." >&2
+    usage
+    exit 1
+fi
+eval set -- "$PARSED_ARGUMENTS"
+while true; do
+    case "$1" in
+        -o|--out-dir)
+            OUT_DIR="$2"
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unexpected argument, script bug? $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$OUT_DIR" == "" ] || [ ! -d "$OUT_DIR" ] || [ ! -z "$(ls -A "$OUT_DIR")" ]; then
+    echo "--out-dir must point to an empty directory."
+    exit 1
+fi
+
+#
+# End stupid args boilerplate
+#
 
 # https://github.com/firecracker-microvm/firecracker/blob/main/tests/README.md
 
@@ -27,6 +72,7 @@ TEST='./integration_tests/performance/test_snapshot.py::test_population_latency[
 # that file. But also, it does this relative to a subdirectory of the CWD.
 # Setting an absolute path doesn't work coz it gets interpreted inside a
 # container.
-# TODO: Get the output location from the user.
-mkdir -p output
-tools/devtool -y test --performance -- -s -m nonci "$TEST" --json-report-file=../output/report.json
+local_out_dir=$(mktemp -d ./tmp.XXXXXXXX)
+tools/devtool -y test --performance -- -s -m nonci "$TEST" \
+    --json-report-file=../"$local_out_dir"/report.json
+mv "$local_out_dir"/* "$OUT_DIR"
