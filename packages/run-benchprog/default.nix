621d6c6f0722b826002adb9eee751df645f90724
Brendan Jackman <jackmanb@google.com>
2026-01-22 15:49:07 +0000

    run-benchprog: Add support to import into Falba DB

--- packages/run-benchprog/default.nix
+++ packages/run-benchprog/default.nix
@@ -4,6 +4,10 @@ pkgs.writeShellApplication {
   # Note we don't include SSH or Nix in the dependencies here. In Google corp
   # the Nix binaries won't work because of fancy environmental user definition
   # magic.
-  runtimeInputs = with pkgs; [ getopt rsync ];
+  runtimeInputs = with pkgs; [
+    getopt
+    rsync
+    falba
+  ];
   text = builtins.readFile ./run-benchprog.sh;
 }
