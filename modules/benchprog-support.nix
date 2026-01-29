72aceee9a4cb56f8180f884e436a643be38993f6
Brendan Jackman <jackmanb@google.com>
2026-01-29 13:40:26 +0000

    nix fmt

--- modules/benchprog-support.nix
+++ modules/benchprog-support.nix
@@ -1,7 +1,7 @@
 # Stuff to set in your test machine's configuration in order to make the basic
 # benchmarking infrastructure work.
 # This should be imported by all the benchmark-specific modules.
-{ self, ...  }:
+{ self, ... }:
 {
   config = {
     # Record the version of the flake, this will then be available
--- packages/wrap-benchmark.nix
+++ packages/wrap-benchmark.nix
@@ -3,11 +3,12 @@
   name,
   rawBenchmark,
   lib,
-  passthru ? {},
+  passthru ? { },
   ...
 }:
 pkgs.writeShellScriptBin "${name}-wrapped" ''
   export KBN_CACHE_DIR=''${XDG_CACHE_HOME:-"$HOME"/.cache}/kbn/${name}
   mkdir -p "$KBN_CACHE_DIR"
   exec "${lib.getExe rawBenchmark}" "$@"
-'' // passthru
+''
+// passthru
