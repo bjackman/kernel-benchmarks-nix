df1a6b6694bddf206b5287e0095975e2d0327674
Brendan Jackman <jackmanb@google.com>
2026-01-29 14:17:55 +0000

    Add hello-world benchmark

--- packages/benchmarks/hello-world.nix
+++ packages/benchmarks/hello-world.nix
@@ -10,9 +10,10 @@ let
 in
 # TODO: Not sure if callPackage is appropriate here of if wrap-benchmark should
 # be a function passed into the package.
-pkgs.callPackage ../../wrap-benchmark.nix {
+pkgs.callPackage ../wrap-benchmark.nix {
   inherit name;
   rawBenchmark = pkgs.writeShellApplication {
+    inherit name;
     text = "echo hello world";
   };
 }
