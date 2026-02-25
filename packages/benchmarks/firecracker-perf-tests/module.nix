1b89419d8fde61ca6975f7cf357d283cad97ef61
Brendan Jackman <jackmanb@google.com>
2026-02-25 14:19:46 +0000

    firecracker-perf-tests: Set up hugepages on boot

--- packages/benchmarks/firecracker-perf-tests/module.nix
+++ packages/benchmarks/firecracker-perf-tests/module.nix
@@ -6,4 +6,8 @@
   # permissions errors, we just need to deal with the fact that it leaves behind
   # files owned by root.
   virtualisation.docker.enable = true;
+
+  # Firecracker's devtool will allocate this many 2M hugepages, to speed that
+  # process up just come up with that many in the first place.
+  boot.kernelParams = [ "default_hugepagesz=2M" "hugepagesz=2M" "hugepages=24552" ];
 }
