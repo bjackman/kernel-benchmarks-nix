13e1feea489b77ad78c7e8e564a902fa994311cd
Brendan Jackman <jackmanb@google.com>
2026-03-13 14:30:59 +0000

    wrapBenchmark: Make base guest module accept args

--- lib/wrapBenchmark.nix
+++ lib/wrapBenchmark.nix
@@ -52,100 +52,103 @@ wrappedProg
             qemuExitPortHex = "0xf4";
           in
           [
-            rec {
-              networking.hostName = hostName;
-              virtualisation.vmVariant = {
-                virtualisation = {
-                  graphics = false;
-                  qemu.options = [
-                    # This BIOS doesn't mess up the terminal and is apparently faster.
-                    "-bios"
-                    "qboot.rom"
-                    "-device"
-                    "isa-debug-exit,iobase=${qemuExitPortHex},iosize=0x04"
-                  ];
-                  # Tell the VM runner script that it should mount a directory on the
-                  # host, named in the environment variable, to /mnt/kbn-output. That
-                  # variable must point to a directory. This is coupled with the script
-                  # content below.
-                  sharedDirectories = {
-                    kbn-output = {
-                      source = "$KBN_OUTPUT_HOST";
-                      target = "/mnt/kbn-output";
+            (
+              { pkgs, config, ... }:
+              rec {
+                networking.hostName = hostName;
+                virtualisation.vmVariant = {
+                  virtualisation = {
+                    graphics = false;
+                    qemu.options = [
+                      # This BIOS doesn't mess up the terminal and is apparently faster.
+                      "-bios"
+                      "qboot.rom"
+                      "-device"
+                      "isa-debug-exit,iobase=${qemuExitPortHex},iosize=0x04"
+                    ];
+                    # Tell the VM runner script that it should mount a directory on the
+                    # host, named in the environment variable, to /mnt/kbn-output. That
+                    # variable must point to a directory. This is coupled with the script
+                    # content below.
+                    sharedDirectories = {
+                      kbn-output = {
+                        source = "$KBN_OUTPUT_HOST";
+                        target = "/mnt/kbn-output";
+                      };
                     };
+                    # Attempt to ensure there's space left over in the rootfs (which
+                    # may be where /tmp is).
+                    diskSize = lib.mkDefault (2 * 1024); # Megabytes
+                    # This seems to speed up boot a bit.
+                    cores = 8;
+                    memorySize = lib.mkDefault (6 * 1024); # Megabytes
                   };
-                  # Attempt to ensure there's space left over in the rootfs (which
-                  # may be where /tmp is).
-                  diskSize = lib.mkDefault (2 * 1024); # Megabytes
-                  # This seems to speed up boot a bit.
-                  cores = 8;
-                  memorySize = lib.mkDefault (6 * 1024); # Megabytes
                 };
-              };
-              boot.tmp.useTmpfs = true;
-              # As an easy way to be able to run it from the kernel cmdline, just
-              # encode the benchmark into a systemd service. You can then run it
-              # with systemd.unit=kbn-guest.service
-              systemd.services.kbn-guest = {
-                script = ''
-                  set +e
-                  ${lib.getExe wrappedProg} --out-dir /mnt/kbn-output
-                  echo $? > /run/kbn-exit-code
-                '';
-                serviceConfig = {
-                  Type = "oneshot";
-                  StandardOutput = "tty";
-                  StandardError = "tty";
-                  CacheDir = "kbn-guest";
+                boot.tmp.useTmpfs = true;
+                # As an easy way to be able to run it from the kernel cmdline, just
+                # encode the benchmark into a systemd service. You can then run it
+                # with systemd.unit=kbn-guest.service
+                systemd.services.kbn-guest = {
+                  script = ''
+                    set +e
+                    ${lib.getExe wrappedProg} --out-dir /mnt/kbn-output
+                    echo $? > /run/kbn-exit-code
+                  '';
+                  serviceConfig = {
+                    Type = "oneshot";
+                    StandardOutput = "tty";
+                    StandardError = "tty";
+                    CacheDir = "kbn-guest";
+                  };
+                  onSuccess = [ "poweroff.target" ];
+                  onFailure = [ "poweroff.target" ];
+                  after = lib.optional requiresInternet "network-online.target";
+                  wants = lib.optional requiresInternet "network-online.target";
                 };
-                onSuccess = [ "poweroff.target" ];
-                onFailure = [ "poweroff.target" ];
-                after = lib.optional requiresInternet "network-online.target";
-                wants = lib.optional requiresInternet "network-online.target";
-              };
-              # This service does the forwarding of the benchmark exit code to
-              # the QEMU hypervisor. I think the proper way to do this would be
-              # /usr/lib/systemd/systemd-shutdown/ but I couldn't get that
-              # working so whatever.
-              systemd.services.kbn-shutdown-exit = {
-                unitConfig.DefaultDependencies = false;
-                script = ''
-                  set +x
-                  if [ -f /run/kbn-exit-code ]; then
-                    CODE=$(cat /run/kbn-exit-code)
-                  else
-                    echo "/run/kbn-exit-code missing"
-                    CODE=213
-                  fi
-                  # Writing the value v to the isa-debug-exit port will cause QEMU to
-                  # immediately exit with the exit code `v << 1 | 1`.
-                  ${pkgs.ioport}/bin/outb ${qemuExitPortHex} $(( CODE - 1 ))
-                '';
-                serviceConfig = {
-                  Type = "oneshot";
-                  StandardOutput = "tty";
-                  StandardError = "tty";
+                # This service does the forwarding of the benchmark exit code to
+                # the QEMU hypervisor. I think the proper way to do this would be
+                # /usr/lib/systemd/systemd-shutdown/ but I couldn't get that
+                # working so whatever.
+                systemd.services.kbn-shutdown-exit = {
+                  unitConfig.DefaultDependencies = false;
+                  script = ''
+                    set +x
+                    if [ -f /run/kbn-exit-code ]; then
+                      CODE=$(cat /run/kbn-exit-code)
+                    else
+                      echo "/run/kbn-exit-code missing"
+                      CODE=213
+                    fi
+                    # Writing the value v to the isa-debug-exit port will cause QEMU to
+                    # immediately exit with the exit code `v << 1 | 1`.
+                    ${pkgs.ioport}/bin/outb ${qemuExitPortHex} $(( CODE - 1 ))
+                  '';
+                  serviceConfig = {
+                    Type = "oneshot";
+                    StandardOutput = "tty";
+                    StandardError = "tty";
+                  };
+                  after = [ "shutdown.target" ];
+                  before = [ "poweroff.target" ];
+                  wantedBy = [ "poweroff.target" ];
                 };
-                after = [ "shutdown.target" ];
-                before = [ "poweroff.target" ];
-                wantedBy = [ "poweroff.target" ];
-              };
-              # Don't bother storing logs to disk, that seems like it will just
-              # occasionally lead to unnecessary slowdowns for log rotation and
-              # stuff.
-              services.journald.storage = "volatile";
+                # Don't bother storing logs to disk, that seems like it will just
+                # occasionally lead to unnecessary slowdowns for log rotation and
+                # stuff.
+                services.journald.storage = "volatile";
 
-              services.getty.autologinUser = "root";
-              services.openssh = {
-                enable = true;
-                settings = {
-                  PermitEmptyPasswords = "yes";
-                  PermitRootLogin = "yes";
+                services.getty.autologinUser = "root";
+                services.openssh = {
+                  enable = true;
+                  settings = {
+                    PermitEmptyPasswords = "yes";
+                    PermitRootLogin = "yes";
+                  };
                 };
-              };
-              users.users.root.initialHashedPassword = "";
-              security.pam.services.sshd.allowNullPassword = true;
-            }
+                users.users.root.initialHashedPassword = "";
+                security.pam.services.sshd.allowNullPassword = true;
+              }
+            )
           ]
           ++ nixosModules;
       };
