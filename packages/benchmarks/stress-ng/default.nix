924a5cb3215360e4620eafcd7e00eb4e69e2ee93
Brendan Jackman <jackmanb@google.com>
2026-03-13 15:25:53 +0000

    benchmarks/stress-ng: Add yq-based falba parser

--- packages/benchmarks/stress-ng/default.nix
+++ packages/benchmarks/stress-ng/default.nix
@@ -18,5 +18,9 @@ wrapBenchmark {
       stress-ng --secretmem 0 -t 5s --metrics-brief --yaml "$OUT_DIR"/stress-ng-metrics-brief.yaml
     '';
   };
+  passthru = rec {
+    falba-parsers = pkgs.callPackage ./falba-parsers.nix { };
+    falba-parsers-json = pkgs.writers.writeJSON "falba-parsers.json" { parsers = falba-parsers.parsers; };
+  };
 }
 
---
+++ packages/benchmarks/stress-ng/falba-parsers.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  parsers.stress_ng_secretmem_bogo_ops_real_time = {
+    type = "command";
+    artifact_regexp = "stress-ng-metrics-brief.yaml";
+    args = [
+      # TODO: https://github.com/bjackman/falba/pull/8
+      "${pkgs.yq}/bin/yq"
+      ".metrics[] | select(.stressor == \"secretmem\") | .\"bogo-ops-per-second-real-time\""
+    ];
+    metric = {
+      name = "stress_ng_secretmem_bogo_ops_real_time";
+      type = "float";
+      unit = "Hz";
+    };
+  };
+  parsers.stress_ng_secretmem_system_time = {
+    type = "command";
+    artifact_regexp = "stress-ng-metrics-brief.yaml";
+    args = [
+      "${pkgs.yq}/bin/yq"
+      ".metrics[] | select(.stressor == \"secretmem\") | .\"system-time\""
+    ];
+    metric = {
+      name = "stress_ng_secretmem_system_time";
+      type = "float";
+      unit = "s";
+    };
+  };
+}
