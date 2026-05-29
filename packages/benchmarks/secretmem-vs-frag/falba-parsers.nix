36bf938ef43db9eaf64dbbcc8463f05d96b6d51f
Brendan Jackman <jackmanb@google.com>
2026-05-29 14:00:09 +0000

    benchmarks/secretmem-vs-frag: Drop iterations fact
    If the benchmark works properly this fact doesn't tell you anything
    about the metrics, it just breaks functional determination and makes it
    harder to use falba cmp.

--- packages/benchmarks/secretmem-vs-frag/falba-parsers.nix
+++ packages/benchmarks/secretmem-vs-frag/falba-parsers.nix
@@ -18,13 +18,4 @@
       type = "bool";
     };
   };
-  parsers.secretmem_iterations = {
-    type = "jsonpath";
-    artifact_regexp = "secretmem_vs_frag_summary\\.json";
-    jsonpath = "$.iterations";
-    fact = {
-      name = "secretmem_iterations";
-      type = "int";
-    };
-  };
 }
