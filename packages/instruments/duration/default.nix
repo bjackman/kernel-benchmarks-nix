ba3178b6429ce194f5dca359c3f31ef86917c866
Brendan Jackman <jackmanb@google.com>
2026-06-04 17:29:11 +0000

    instruments/duration: Deslop nix code

--- packages/instruments/duration/default.nix
+++ packages/instruments/duration/default.nix
@@ -1,26 +1,23 @@
 { pkgs, ... }:
-let
-  drv = pkgs.writeShellApplication {
-    name = "duration";
-    runtimeInputs = with pkgs; [
-      coreutils
-    ];
-    text = builtins.readFile ./duration.sh;
+pkgs.writeShellApplication {
+  name = "duration";
+  runtimeInputs = with pkgs; [
+    coreutils
+  ];
+  text = builtins.readFile ./duration.sh;
 
-    passthru = rec {
-      falba-parsers.parsers.duration = {
-        type = "single_metric";
-        artifact_regexp = "instrumentation/duration/duration.txt";
-        metric = {
-          name = "duration";
-          type = "int";
-          unit = "us";
-        };
-      };
-      falba-parsers-json = pkgs.writers.writeJSON "falba-parsers.json" {
-        parsers = falba-parsers.parsers;
+  passthru = rec {
+    falba-parsers.parsers.duration = {
+      type = "single_metric";
+      artifact_regexp = "instrumentation/duration/duration.txt";
+      metric = {
+        name = "duration";
+        type = "int";
+        unit = "us";
       };
     };
+    falba-parsers-json = pkgs.writers.writeJSON "falba-parsers.json" {
+      parsers = falba-parsers.parsers;
+    };
   };
-in
-drv
+}
