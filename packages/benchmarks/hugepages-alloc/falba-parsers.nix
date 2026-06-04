cbfd6bd6db02c45db76d1cb20aa641d9c8a82687
Brendan Jackman <jackmanb@google.com>
2026-06-04 17:13:34 +0000

    benchmarks/hugepages-alloc: Fixup parser
    Make allocated a metric (it's an "output"). Add parser for requested.

--- packages/benchmarks/hugepages-alloc/falba-parsers.nix
+++ packages/benchmarks/hugepages-alloc/falba-parsers.nix
@@ -1,11 +1,22 @@
 {
-  parsers.allocated_hugepages = {
-    type = "jsonpath";
-    artifact_regexp = "hugepages_alloc_summary\\.json";
-    jsonpath = "$.allocated_hugepages";
-    fact = {
-      name = "allocated_hugepages";
-      type = "int";
+  parsers = {
+    requested_hugepages = {
+      type = "jsonpath";
+      artifact_regexp = "hugepages_alloc_summary\\.json";
+      jsonpath = "$.requested_hugepages";
+      fact = {
+        name = "requested_hugepages";
+        type = "int";
+      };
+    };
+    allocated_hugepages = {
+      type = "jsonpath";
+      artifact_regexp = "hugepages_alloc_summary\\.json";
+      jsonpath = "$.allocated_hugepages";
+      metric = {
+        name = "allocated_hugepages";
+        type = "int";
+      };
     };
   };
 }
