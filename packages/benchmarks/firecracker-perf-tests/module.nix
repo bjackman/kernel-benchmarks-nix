650f4588858ef0acbcbe311dff1917cdf5eabd6f
Brendan Jackman <jackmanb@google.com>
2026-06-04 16:00:19 +0000

    benchmarks/firecracker-perf-test: Notes on nr_hugepages setup

--- packages/benchmarks/firecracker-perf-tests/module.nix
+++ packages/benchmarks/firecracker-perf-tests/module.nix
@@ -7,10 +7,24 @@
   # files owned by root.
   virtualisation.docker.enable = true;
 
-  virtualisation.vmVariant.virtualisation = {
-    # Need to download container images, big disk when testing in a VM plz.
-    diskSize = 16 * 1024; # Megabytes
-    # Also this is a big workload, much RAM
-    memorySize = 64 * 1024;
+  virtualisation.vmVariant = {
+    # The firecracker devtool will allocate 48G of 2M hugepages which is really
+    # slow in a VM.
+    # Attempts to fix this:
+    # This just causes VM boot to be really slow:
+    # boot.kernelParams = [ "hugepagesz=2M" "hugepages=24552"];
+    # This causes the docker process to crash (presumably getting OOMed, I
+    # suspect because there is basically no memory available for normal non-huge
+    # pages or something):
+    # boot.kernelParams = [ "hugepagesz=1G" "hugepages=48"];
+    # After chatting to Frank van der Linden I realised it's probably a bug that
+    # allocating those hugepages takes so long anyway.
+
+    virtualisation = {
+      # Need to download container images, big disk when testing in a VM plz.
+      diskSize = 16 * 1024; # Megabytes
+      # Also this is a big workload, much RAM
+      memorySize = 64 * 1024;
+    };
   };
 }
