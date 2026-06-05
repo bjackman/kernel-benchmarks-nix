ca016366cdc1e0e4e5498138747a6aec4987ab73
Brendan Jackman <jackmanb@google.com>
2026-06-05 13:01:57 +0000

    benchmarks/hugepages-alloc: Add --size arg

--- packages/benchmarks/hugepages-alloc/default.nix
+++ packages/benchmarks/hugepages-alloc/default.nix
@@ -1,5 +1,7 @@
 # This benchmark just does a single allocation of a bunch of hugepages by
 # writing nr_hugepages in sysfs.
+# --size sets the overall size of the hugepages to allocate (rounded up to size
+# of hugepage). --nr-hugepages instead sets the count of pages to allocate.
 {
   pkgs,
   lib,
@@ -12,10 +14,62 @@ let
   rawBenchmark = pkgs.writeShellApplication {
     inherit name;
     text = ''
-            NR_HUGEPAGES=24552
+            parse_size_to_hugepages() {
+                local size_str="$1"
+                # Remove trailing whitespace
+                size_str=$(echo "$size_str" | tr -d '[:space:]')
+
+                # Extract number and suffix
+                if [[ "$size_str" =~ ^([0-9]+)([kKmMgGtT]?[bB]?)$ ]]; then
+                    local num="''${BASH_REMATCH[1]}"
+                    local suffix="''${BASH_REMATCH[2]}"
+                else
+                    echo "Error: Invalid size format: $size_str" >&2
+                    exit 1
+                fi
+
+                local multiplier=1
+                case "$suffix" in
+                    [kK]|[kK][bB])
+                        multiplier=1024
+                        ;;
+                    [mM]|[mM][bB])
+                        multiplier=$((1024 * 1024))
+                        ;;
+                    [gG]|[gG][bB])
+                        multiplier=$((1024 * 1024 * 1024))
+                        ;;
+                    [tT]|[tT][bB])
+                        multiplier=$((1024 * 1024 * 1024 * 1024))
+                        ;;
+                    "")
+                        multiplier=1
+                        ;;
+                    *)
+                        echo "Error: Unsupported suffix: $suffix" >&2
+                        exit 1
+                        ;;
+                esac
+
+                local total_bytes
+                total_bytes=$((num * multiplier))
+
+                local hp_size=$((2 * 1024 * 1024)) # 2MB
+
+                local nr_hugepages=$(( (total_bytes + hp_size - 1) / hp_size ))
+
+                echo "$nr_hugepages"
+            }
+
+            SIZE=""
+            NR_HUGEPAGES=""
 
             while [ $# -gt 0 ]; do
                 case "$1" in
+                    -s|--size)
+                        SIZE="$2"
+                        shift 2
+                        ;;
                     -n|--nr-hugepages)
                         NR_HUGEPAGES="$2"
                         shift 2
@@ -25,6 +79,19 @@ let
                         ;;
                 esac
             done
+
+            if [ -n "$SIZE" ] && [ -n "$NR_HUGEPAGES" ]; then
+                echo "--size and --nr-hugepages are mutually exclusive"
+                exit 1
+            fi
+
+            if [ -z "$SIZE" ] && [ -z "$NR_HUGEPAGES" ]; then
+                NR_HUGEPAGES=24552
+            fi
+
+            if [ -n "$SIZE" ]; then
+                NR_HUGEPAGES=$(parse_size_to_hugepages "$SIZE")
+            fi
 
             HUGEPAGES_FILE="/sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages"
 
